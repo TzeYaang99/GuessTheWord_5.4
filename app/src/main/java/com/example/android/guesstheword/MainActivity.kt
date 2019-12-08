28ecfc63300cfe209a8efd4f39b98e9631958b8f
zhe991023@gmail.com <zhe991023@gmail.com>
2019-12-08 20:25:39 +0800

    initial

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -26,6 +26,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+
     }
 
 }
